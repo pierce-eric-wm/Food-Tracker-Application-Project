14d1b08091af413a92d9307b06dd27a7926bc9a8
Eric <eric-pierce@west-mec.org>
2015-11-02 12:58:51 -0700

    not done but i need to turn in

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 //add numbers counting up to number the products
-function newRow() {
-    var table = document.getElementById("container2");
-    var row = table.insertRow(0);
-    var cell1 = row.insertRow(0);
-    var cell2 = row.insertRow(1);
-    var cell3 = row.insertRow(2);
-    var cell4 = row.insertRow(3);
-    var number = document.getElementById("number").value;
-    var newConent = document.CreateTextNode(number);
-    cell1.appendChild(newContent);
-    //next
+function newRow (){
+
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(1);
+    var cell1= row.insertCell(0);
+    var cell2= row.insertCell(1);
+    var cell3= row.insertCell(2);
+    var cell4= row.insertCell(3);
+
     var foodName = document.getElementById("foodName").value;
-    var newConent2 = document.CreateTextNode(foodName);
+    var newContent = document.createTextNode(foodName);
+    cell1.appendChild(newContent);
+
+
+    var description = document.getElementById("description").value;
+    var newContent2 = document.createTextNode(description);
     cell2.appendChild(newContent2);
-    //next
-    var purchaseDate = document.getElementById("purchase").value;
-    var newConent3 = document.CreateTextNode(purchase);
+
+    var purchase = document.getElementById("purchase").value;
+    var newContent3 = document.createTextNode(purchase);
     cell3.appendChild(newContent3);
-    //next
-    var expDate = document.getElementById("expiration").value;
-    var newConent4 = document.CreateTextNode(expiration);
+
+    var expire = document.getElementById("expire").value;
+    var newContent4 = document.createTextNode(expire);
+    var newClass= document.getElementById("expire");
+    cell4.setAttribute('class',newClass);
     cell4.appendChild(newContent4);
-    //next
+
 }
